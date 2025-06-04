01ecc175afc08f4ad4aa5cfd66aee2e48aa4550e
Rafael Carvalho <rafael_hosanah5@hotmail.com>
2025-06-04 12:13:24 -0300

    Update Sales.jsx

--- frontend/src/pages/sales/Sales.jsx
+++ frontend/src/pages/sales/Sales.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { AuthContext } from '../../contexts/AuthContext';
+import AuthContext from '@/contexts/AuthContext';
 import api from '../../services/api';
 import {
   DollarSign,
