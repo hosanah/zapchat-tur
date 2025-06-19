aa3614f031eeed7ab198935a4ce537969525c15a
stuartffh <91503509+stuartffh@users.noreply.github.com>
2025-06-19 14:03:13 -0300

    fix(settings): merge logo upload validation

--- backend/src/routes/settings.js
+++ backend/src/routes/settings.js
@@ -8,9 +8,8 @@ const generalSettingValidations = require('../middleware/generalSettingValidatio
 router.use(authenticate);
 
 router.get('/', GeneralSettingController.get);
-router.put('/', uploadLogo, GeneralSettingController.update);
+router.put('/', uploadLogo, generalSettingValidations.update, GeneralSettingController.update);
 router.post('/', generalSettingValidations.create, GeneralSettingController.create);
-router.put('/', generalSettingValidations.update, GeneralSettingController.update);
 router.delete('/', GeneralSettingController.delete);
 
 module.exports = router;
